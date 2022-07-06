762d394ace3670febe4c9212dc88bdcfc34bf000
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-07-06 11:30:34 +0200

    add display function

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ function addListeners() {
     document.querySelectorAll("button").forEach(item => {
         item.addEventListener("click", event => {
             if (item.hasAttribute('data-digit')) {
+                console.log("data-digit exists");
+                display(item.getAttribute('data-digit'));
                 return Number.parseInt(item.getAttribute('data-digit'));
+            } else if (item.hasAttribute('data-op')) {
+                console.log("data-op exists");
+                return item.getAttribute('data-op');
             }
         })
     }
     )
-}
+};
 
 addListeners();
+
+function display(clicked) {
+    document.getElementById("display").innerText += clicked;
+}
+
