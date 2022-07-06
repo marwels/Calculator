8334652e536c15b02684b62f691838f0e6ef44b9
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-07-06 14:53:22 +0200

    change all Number.parseInt() into Number()
    it allows for numbers with decimals

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addListeners() {
             if (item.hasAttribute('data-digit')) {
                 console.log("data-digit exists");
                 display(item.getAttribute('data-digit'));
-                // return Number.parseInt(item.getAttribute('data-digit'));
+                // return Number(item.getAttribute('data-digit'));
             } else if (item.hasAttribute('data-op')) {
                 console.log("data-op exists");
                 operation(item.getAttribute('data-op'));
@@ -53,22 +53,23 @@ function operation(clicked) {
             document.getElementById("display").innerText = displayValue;
         }
     } else if (clicked === "division") {
-        numberA = Number.parseInt(displayValue);
+        numberA = Number(displayValue);
         operationToCompute = "division";
         displayValue = "0";
     } else if (clicked === "multiplication") {
-        numberA = Number.parseInt(displayValue);
+        numberA = Number(displayValue);
         operationToCompute = "multiplication";
         displayValue = "0";
     } else if (clicked === "subtraction") {
-        numberA = Number.parseInt(displayValue);
+        numberA = Number(displayValue);
         operationToCompute = "subtraction";
         displayValue = "0";
     } else if (clicked === "addition") {
-        numberA = Number.parseInt(displayValue);
+        numberA = Number(displayValue);
         operationToCompute = "addition";
         displayValue = "0";
     } else if (clicked === "dot") {
+        display(".");
     } else if (clicked === "sum") {
         if (operationToCompute === "") {
             return;
@@ -91,7 +92,7 @@ function division() {
         document.getElementById("display").innerText = "ERROR: DON'T DIVIDE BY 0!"
         return;
     }
-    let numberB = Number.parseInt(displayValue);
+    let numberB = Number(displayValue);
     let outcome = numberA / numberB;
     displayValue = outcome;
     document.getElementById("display").innerText = displayValue;
@@ -99,7 +100,7 @@ function division() {
 }
 
 function multiplication() {
-    let numberB = Number.parseInt(displayValue);
+    let numberB = Number(displayValue);
     let outcome = numberA * numberB;
     displayValue = outcome;
     document.getElementById("display").innerText = displayValue;
@@ -107,7 +108,7 @@ function multiplication() {
 }
 
 function subtraction() {
-    let numberB = Number.parseInt(displayValue);
+    let numberB = Number(displayValue);
     let outcome = numberA - numberB;
     displayValue = outcome;
     document.getElementById("display").innerText = displayValue;
@@ -115,7 +116,7 @@ function subtraction() {
 }
 
 function addition() {
-    let numberB = Number.parseInt(displayValue);
+    let numberB = Number(displayValue);
     let outcome = numberA + numberB;
     displayValue = outcome;
     document.getElementById("display").innerText = displayValue;
