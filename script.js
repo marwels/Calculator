6eaa31527354a003ee5d50bc4a24b075ec4ebd34
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-07-06 12:55:32 +0200

    add operation() framework

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function addListeners() {
             if (item.hasAttribute('data-digit')) {
                 console.log("data-digit exists");
                 display(item.getAttribute('data-digit'));
-                return Number.parseInt(item.getAttribute('data-digit'));
+                // return Number.parseInt(item.getAttribute('data-digit'));
             } else if (item.hasAttribute('data-op')) {
                 console.log("data-op exists");
                 return item.getAttribute('data-op');
@@ -32,6 +32,19 @@ function display(clicked) {
 
 function operation(clicked) {
     let numberA = Number.parseInt(displayValue);
-    //let toCompute = numberA +
+    if (item.getAttribute('data-op') === "clear") {
+        displayValue = "0";
+    } else if (item.getAttribute('data-op') === "delete") {
+        let sliced = displayValue.slice(0, -1);
+        displayValue = sliced;
+    } else if (item.getAttribute('data-op') === "division") {
+    } else if (item.getAttribute('data-op') === "multiplication") {
+    } else if (item.getAttribute('data-op') === "subtraction") {
+    } else if (item.getAttribute('data-op') === "addition") {
+    } else if (item.getAttribute('data-op') === "dot") {
+    } else if (item.getAttribute('data-op') === "sum") {
+    } else {
+        displayValue = "error";
+    }
 
 }
