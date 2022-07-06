541b94d68b0a63c9e5aef78bbf96cc46530fa37b
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-07-06 10:51:50 +0200

    change width of calculator

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let displayValue = 0;
 
-addEventListener
+function addListeners() {
+    document.querySelectorAll("button").forEach(item => {
+        item.addEventListener("click", event => {
+            if (item.hasAttribute('data-digit')) {
+                return Number.parseInt(item.getAttribute('data-digit'));
+            }
+        })
+    }
+    )
+}
+
+addListeners();
